13ea47207e1da3151e8c22833137f9de9be9947a
Zolboo21 <zolboo.oz@gmail.com>
2023-05-12 16:39:20 +0900

    using blinding

--- WpfApp1/WpfApp1/ViewModel/ViewModelClass.cs
+++ WpfApp1/WpfApp1/ViewModel/ViewModelClass.cs
@@ -1,12 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace WpfApp1
 {
-    internal class ViewModelClass
+    class ViewModelClass : INotifyPropertyChanged
     {
+        private int red;
+        public int Red
+        {
+            get { return red; }
+            set
+            {
+                red = value;
+                OnPropertyChanged("Red");
+                OnColorChanged();
+            }
+        }
+        private int green;
+        public int Green
+        {
+            get { return green; }
+            set
+            {
+                green = value;
+                OnPropertyChanged("Green");
+                OnColorChanged();
+            }
+        }
+
+        private int blue;
+        public int Blue
+        {
+            get { return blue; }
+            set
+            {
+                blue = value;
+                OnPropertyChanged("Blue");
+                OnColorChanged();
+            }
+        }
+
+
+
+
+        private SolidColorBrush colorBrush;
+        public SolidColorBrush ColorBrush
+        {
+            get { return colorBrush; }
+            set
+            {
+                colorBrush = value;
+                OnPropertyChanged("ColorBrush");
+            }
+        }
+
+
+
+    private void OnColorChanged()
+    {
+            ColorBrush = new SolidColorBrush(Color.FromRgb((byte)red, (byte)green, (byte)blue));
+
+    }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
     }
 }

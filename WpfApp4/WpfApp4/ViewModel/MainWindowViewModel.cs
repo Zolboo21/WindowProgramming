1a8a9c7e5632db17001c7d8ff11ee68d71dfc66f
Zolboo21 <zolboo.oz@gmail.com>
2023-05-19 13:34:13 +0900

    button

---
+++ WpfApp4/WpfApp4/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WpfApp4.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        ICommand buttonCommand;
+
+        public ICommand ButtonCommand
+        {
+            get
+            {
+                return buttonCommand;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            buttonCommand = new RelayCommand(ExecuteCommand);
+        }
+
+        private void ExecuteCommand()
+        {
+            MessageBox.Show("Receive Command");
+        }
+    }
+}

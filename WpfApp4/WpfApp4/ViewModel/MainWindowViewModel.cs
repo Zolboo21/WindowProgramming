b65baf294b737fadba899d420094788f50eb1ae8
Zolboo21 <zolboo.oz@gmail.com>
2023-05-19 14:17:50 +0900

    .

--- WpfApp4/WpfApp4/ViewModel/MainWindowViewModel.cs
+++ WpfApp4/WpfApp4/ViewModel/MainWindowViewModel.cs
@@ -5,30 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using CommunityToolkit.Mvvm.Input;
 
 namespace WpfApp4.ViewModel
 {
-    class MainWindowViewModel : ViewModelBase
+    class Car
     {
-        ICommand buttonCommand;
-
-        public ICommand ButtonCommand
+        public String Company { get; set; }
+        public String Model { get; set; }
+        public String Year { get; set; }
+        public Color CarColor { get; set; }
+    }
+        class MainWindowViewModel
         {
-            get
+            List<Car> _cars;
+
+            public List<Car> Cars
             {
-                return buttonCommand;
+                get
+                {
+                    return _cars;
+                }
+            }
+
+            public MainWindowViewModel()
+            {
+                _cars = new List<Car>();
+
+                _cars.Add(new Car() { Company = "Benz", Model = "Gle", Year = "1996", CarColor = Colors.Aqua });
+                _cars.Add(new Car() { Company = "Bmw", Model = "x5", Year = "2011", CarColor = Colors.Blue });
+                _cars.Add(new Car() { Company = "Hyundae", Model = "Sonata", Year = "2011", CarColor = Colors.Gold });
             }
         }
-
-        public MainWindowViewModel()
-        {
-            buttonCommand = new RelayCommand(ExecuteCommand);
-        }
-
-        private void ExecuteCommand()
-        {
-            MessageBox.Show("Receive Command");
-        }
-    }
 }

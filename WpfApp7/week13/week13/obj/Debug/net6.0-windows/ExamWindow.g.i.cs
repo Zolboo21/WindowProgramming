f09f42fe57737934ca8442f454e8ac730c17b9a5
Zolboo21 <zolboo.oz@gmail.com>
2023-05-26 16:01:12 +0900

    s

--- WpfApp7/week13/week13/obj/Debug/net6.0-windows/ExamWindow.g.i.cs
+++ WpfApp7/week13/week13/obj/Debug/net6.0-windows/ExamWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace week13 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/week13;component/examwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/week13;V1.0.0.0;component/examwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ExamWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

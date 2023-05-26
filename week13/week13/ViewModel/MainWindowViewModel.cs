7d8668b19757d12403699c82cb42c7c63bbfee75
Zolboo21 <zolboo.oz@gmail.com>
2023-05-26 15:16:03 +0900

    s

---
+++ week13/week13/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week13.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+
+
+        RelayCommand _OpenExamWindow;
+        public RelayCommand OpenExamWindow
+        {
+            get
+            {
+                return _OpenExamWindow;
+            }
+        }
+        public MainWindowViewModel()
+        {
+            _OpenExamWindow = new RelayCommand(OpenExamWindowExcute);
+        }
+
+        public void OpenExamWindowExcute()
+        {
+            
+        }
+    }
+    
+    }
+
---
+++ week13/week13/ViewModel/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week13.ViewModel
+{
+    internal class ViewModelBase : INotifyPropertyChanging
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+    }
+}

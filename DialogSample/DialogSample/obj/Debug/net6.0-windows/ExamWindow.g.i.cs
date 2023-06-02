d28498506fb6a7b2b36d5319410de90a23559dea
Zolboo21 <zolboo.oz@gmail.com>
2023-06-02 15:49:25 +0900

    x

--- DialogSample/DialogSample/obj/Debug/net6.0-windows/ExamWindow.g.i.cs
+++ DialogSample/DialogSample/obj/Debug/net6.0-windows/ExamWindow.g.i.cs
@@ -78,7 +78,7 @@ namespace DialogSample {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DialogSample;component/examwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DialogSample;V1.0.0.0;component/examwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ExamWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- DialogSample/DialogSample/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ DialogSample/DialogSample/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -78,7 +78,7 @@ namespace DialogSample {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DialogSample;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DialogSample;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
